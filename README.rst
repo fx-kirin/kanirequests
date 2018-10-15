6fb2012fd1252df467f9fe88df2e5f91154d027a
Yoshiaki Ono <ono.kirin@gmail.com>
2018-10-15 10:36:47 +0900

    1st

---
+++ README.rst
@@ -0,0 +1,32 @@
+pyhttp
+======
+
+.. image:: https://img.shields.io/pypi/v/pyhttp.svg
+    :target: https://pypi.python.org/pypi/pyhttp
+    :alt: Latest PyPI version
+
+.. image:: ''.png
+   :target: ''
+   :alt: Latest Travis CI build status
+
+''
+
+Usage
+-----
+
+Installation
+------------
+
+Requirements
+^^^^^^^^^^^^
+
+Compatibility
+-------------
+
+Licence
+-------
+
+Authors
+-------
+
+`pyhttp` was written by `fx-kirin <ono.kirin@gmail.com>`_.
---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+setuptools.setup(
+    name="pyhttp",
+    version="0.1.0",
+    url="''",
+
+    author="fx-kirin",
+    author_email="ono.kirin@gmail.com",
+
+    description="''",
+    long_description=open('README.rst').read(),
+
+    packages=setuptools.find_packages(),
+
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
